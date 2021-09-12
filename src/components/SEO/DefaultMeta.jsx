cd659f98e8fdc5c91f0942dcd11356e4d06d85b4
Vladimir Skok <iamskok@users.noreply.github.com>
2021-09-11 21:21:24 -0400

    fix: html tag lang format

--- src/components/SEO/DefaultMeta.jsx
+++ src/components/SEO/DefaultMeta.jsx
@@ -1,13 +1,17 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 
-const DefaultMeta = ({ title, description, language, url }) => (
-  <Helmet>
-    <html lang={language} />
-    <title>{title}</title>
-    <meta name="description" content={description} />
-    <link rel="canonical" href={url} />
-  </Helmet>
-)
+const DefaultMeta = ({ title, description, language, url }) => {
+  const lang = language.split(`_`)[0]
+
+  return (
+    <Helmet>
+      <html lang={lang} />
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <link rel="canonical" href={url} />
+    </Helmet>
+  )
+}
 
 export default DefaultMeta
